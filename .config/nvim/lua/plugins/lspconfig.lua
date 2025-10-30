cf5fb66a3593ac688eefaaedaddf1fec7f129501
Daniel <daniel.guo@vivalabs.com.au>
2025-10-30 14:18:28 +1100

    Sync config

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,7 +2,38 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      inlay_hints = { enabled = false },
+      inlay_hints = { enabled = true },
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              usePlaceholders = false, -- Disable function parameter placeholders
+            },
+          },
+        },
+
+        ts_ls = {
+          settings = {
+            typescript = {
+              preferences = {
+                includeCompletionsWithSnippetText = false,
+              },
+            },
+          },
+        },
+
+        rust_analyzer = {
+          settings = {
+            ["rust_analyzer"] = {
+              completion = {
+                callable = {
+                  snippets = "add_parentheses",
+                },
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
